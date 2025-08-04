1224faf3d696234d45a150ce72c755908a994275
jayden-leig <ST10315068@vcconnect.edu.za>
2025-08-04 14:13:52 +0200

    added program code

--- IndexIceTaskPart1/Program.cs
+++ IndexIceTaskPart1/Program.cs
@@ -8,23 +8,24 @@
 
             // Uncomment the following lines to test the Fitness class and its indexers
 
-            //Fitness fit = new Fitness(2, 500, "VirginActive", "Adidas", "GreenAcres");
+            Fitness fit = new Fitness(2, 500, "VirginActive", "Adidas", "GreenAcres");
+            new Fitness(4, 700, "Ignite", "Nike", "GreenAcres");
 
-            //fit[2] = "VirginActive";
+            fit[2] = "VirginActive";
 
-            //Console.WriteLine(fit[0]);
-            //Console.WriteLine(fit[1]);
-            //Console.WriteLine(fit[2]);
-            //Console.WriteLine(fit[3]);
-            //Console.WriteLine(fit[4]);
+            Console.WriteLine(fit[0]);
+            Console.WriteLine(fit[1]);
+            Console.WriteLine(fit[2]);
+            Console.WriteLine(fit[3]);
+            Console.WriteLine(fit[4]);
 
-            //fit["fitPrice"] = 500;
+            fit["fitPrice"] = 500;
 
-            //Console.WriteLine("Fitness Package: " + fit["fitpackage"]);
-            //Console.WriteLine("Fitness Price: " + fit["fitprice"]);
-            //Console.WriteLine("Gym Chain: " + fit["gymchain"]);
-            //Console.WriteLine("Gym Brand: " + fit["gymbrand"]);
-            //Console.WriteLine("Gym Location: " + fit["gymlocation"]);
+            Console.WriteLine("Fitness Package: " + fit["fitpackage"]);
+            Console.WriteLine("Fitness Price: " + fit["fitprice"]);
+            Console.WriteLine("Gym Chain: " + fit["gymchain"]);
+            Console.WriteLine("Gym Brand: " + fit["gymbrand"]);
+            Console.WriteLine("Gym Location: " + fit["gymlocation"]);
         }
     }
 }
