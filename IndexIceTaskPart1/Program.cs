612fd22f0f1ff7012d2936962ac9520946cbb221
St10438964 <St10438964@vcconnect.edu.za>
2025-08-04 13:15:39 +0200

    added

--- IndexIceTaskPart1/Program.cs
+++ IndexIceTaskPart1/Program.cs
@@ -5,6 +5,26 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Fitness Package Indexer Demo!");
+
+            // Uncomment the following lines to test the Fitness class and its indexers
+
+            //Fitness fit = new Fitness(2, 500, "VirginActive", "Adidas", "GreenAcres");
+
+            //fit[2] = "VirginActive";
+
+            //Console.WriteLine(fit[0]);
+            //Console.WriteLine(fit[1]);
+            //Console.WriteLine(fit[2]);
+            //Console.WriteLine(fit[3]);
+            //Console.WriteLine(fit[4]);
+
+            //fit["fitPrice"] = 500;
+
+            //Console.WriteLine("Fitness Package: " + fit["fitpackage"]);
+            //Console.WriteLine("Fitness Price: " + fit["fitprice"]);
+            //Console.WriteLine("Gym Chain: " + fit["gymchain"]);
+            //Console.WriteLine("Gym Brand: " + fit["gymbrand"]);
+            //Console.WriteLine("Gym Location: " + fit["gymlocation"]);
         }
     }
 }
